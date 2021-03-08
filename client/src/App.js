b3f4aba123b5a481c4658099ccf4f6b190dd5afd
bettycode <betty.haile.us@gmail.com>
2021-03-08 18:24:14 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>hello</h2>
+     </div>
+  );
+}
+
+
+export default App;
